3f83a01fd5ccd70e0d0d670966f10ad44133c4ea
Md Rashed Ali <tools.rashed@gmail.com>
2023-08-31 20:07:07 +0600

    Category Tabs Added with API

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Linked With PH TUBE JS")
+console.log("Linked With PH TUBE JS");
+
+const getWebData = async () => {
+  let dataUrl = "https://openapi.programming-hero.com/api/videos/categories";
+  const res = await fetch(dataUrl);
+  const rawData = await res.json();
+  const data = rawData.data;
+  let categoryContainer = document.querySelector(".category-container");
+  data.forEach((category) => {
+    console.log(category);
+    let categoryItem = document.createElement("a");
+    categoryItem.classList = "bg-[#25252520] py-2 px-7 rounded text-[#25252590] font-bold"
+    categoryItem.innerText = `${category.category}`;
+    categoryContainer.appendChild(categoryItem);
+  });
+};
+
+getWebData();
